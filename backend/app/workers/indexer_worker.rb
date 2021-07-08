cf732b95637f6fdce51bf0445084f49aad8b27c1
max <sviechnikov@gmail.com>
2021-07-08 14:57:25 +0300

    [#1] rubocop improvements

--- backend/app/workers/indexer_worker.rb
+++ backend/app/workers/indexer_worker.rb
@@ -11,7 +11,7 @@ class IndexerWorker
     version_params = data.slice(*description_fields).merge(package_id: args[:package_id])
 
     version = ::Version.find_or_initialize_by(version_params.slice(:package_id, :number))
-    find_or_create_contributors(data[:maintainers]).each{ |memo| version.maintainers << memo }
+    find_or_create_contributors(data[:maintainers]).each { |memo| version.maintainers << memo }
     find_or_create_contributors(data[:authors]).each { |memo| version.authors << memo }
     version.attributes = version_params
     version.save
@@ -23,6 +23,7 @@ class IndexerWorker
     data.map do |item|
       contr = ::Contributor.find_or_initialize_by(name: item[:name])
       next(contr) if contr.email.present? && contr.name.present?
+
       contr.email = item[:email] if item[:email].present?
       contr.save
       contr
