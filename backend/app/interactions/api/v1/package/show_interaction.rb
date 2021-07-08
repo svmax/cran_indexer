b9f2248e4a3ae16aff0978f089a628d212264017
max <sviechnikov@gmail.com>
2021-07-09 01:06:08 +0300

    [#1] fixing of linter, show method improvements

--- backend/app/interactions/api/v1/package/show_interaction.rb
+++ backend/app/interactions/api/v1/package/show_interaction.rb
@@ -7,7 +7,36 @@ module Api
         string :id
 
         def execute
-          ::Package.find(id)
+          serialize ::Package.find(id)
+        end
+
+        private
+
+        def serialize(item)
+          {
+            name: item.name,
+            id: item.id.to_s,
+            versions: extract_versions(item)
+          }
+        end
+
+        def extract_versions(package)
+          package.versions.includes(:authors, :maintainers).map do |version|
+            maintainers = version.maintainers.map(&method(:extract_contributor))
+            authors = version.authors.map(&method(:extract_contributor))
+            {
+              title: version.title,
+              number: version.number,
+              description: version.description,
+              published_at: version.published_at,
+              maintainers: maintainers,
+              authors: authors
+            }
+          end
+        end
+
+        def extract_contributor(item)
+          { name: item.name, email: item.email }
         end
       end
     end
